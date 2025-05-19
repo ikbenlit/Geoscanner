4618d26d290d318c02707f6b02909a8e7d7c8a1b
ikbenlit <colin@ikbenlit.nl>
2025-05-19 16:35:07 +0200

    eslint.ignoreDuringBuilds: true

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Tijdens de build worden de ESLint fouten behandeld als waarschuwingen
+    ignoreDuringBuilds: true,
+  },
+};
 
 module.exports = nextConfig;
